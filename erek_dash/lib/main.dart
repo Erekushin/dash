97569d8536997fe7957b67cfc291b8896d2f482e
Erek <erekushinforlife@gmail.com>
2023-09-27 07:11:57 +0800

    a

--- erek_dash/lib/main.dart
+++ erek_dash/lib/main.dart
@@ -14,6 +14,7 @@ import 'globals.dart';
 import 'home.dart';
 
 void main() async {
+  // firebase version
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
