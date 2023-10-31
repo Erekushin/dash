5a172d993cbe9c40f3a96fc43ddeac04a131d31c
Erekushin <Erekushinforlife@gmail.com>
2023-10-31 09:29:01 +0800

    a

--- erek_dash/lib/main.dart
+++ erek_dash/lib/main.dart
@@ -15,7 +15,7 @@ import 'home.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-  //web
+  //firebase version
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([
